1311112ac05cd705316afeb7b2320a4dc940a1c7
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-03-22 21:16:21 -0700

    remove unused utils

--- mp3_to_mp4/utils.py
+++ mp3_to_mp4/utils.py
@@ -31,24 +31,3 @@ def get_folder_list(path: Path, type_function) -> int:
   
 def clean_string(string: str):
   return re.sub(r"\W+", "-", string)
-
-  # def check_params(self, bg_color: str, width: int, height: int):
-  #   checks = [
-  #   self.__check_color(bg_color),
-  #   self.__check_dimension(width),
-  #   self.__check_dimension(height),
-  #   ]
-  #   if all([True if check == SUCCESS else False for check in checks]):
-  #     return SUCCESS
-  #   return CONFIG_PARAM_ERROR
-
-  # def __check_color(self, color: str):
-  #   hex_regex = re.compile(r'^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')
-  #   if hex_regex.fullmatch(color) == None:
-  #     return CONFIG_PARAM_ERROR
-  #   return SUCCESS
-
-  # def __check_dimension(self, px: int):
-  #   if 0 < px < 3841:
-  #     return SUCCESS
-  #   return CONFIG_PARAM_ERROR
